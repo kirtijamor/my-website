c0088a4b0668e4e416ddaf52efafb92dee107aa0
kirtijamor <morkirtija@gmail.com>
2018-12-07 16:44:20 +0530

    adding folders to assets , main , about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule
