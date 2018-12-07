0f41d559351805955c91bf8b736efe808d886d40
kirtijamor <morkirtija@gmail.com>
2018-12-07 17:13:49 +0530

    travel-box -> hotels, flights components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { TravelBoxComponent } from './travel-box/travel-box.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { FlightsComponent } from './flights/flights.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
     AboutUsComponent,
-    TravelBoxComponent
+    TravelBoxComponent,
+    HotelsComponent,
+    FlightsComponent
   ],
   imports: [
     BrowserModule
